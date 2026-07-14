c7a7a3bfcf34304762fabd619df22ce016129bc5
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2026-07-14 11:15:19 +0800

    Add my own version of the recursive solution

--- Day23/Part2.py
+++ Day23/Part2.py
@@ -1,3 +1,4 @@
+'''
 def bron_kerbosch(current_clique, candidates, processed_vertices, graph):
 	if not candidates and not processed_vertices:
 		if len(current_clique) > 2:
@@ -30,6 +31,41 @@ current_clique = set()
 candidates = set(graph.keys())
 processed_vertices = set()
 bron_kerbosch(current_clique, candidates, processed_vertices, graph)
+ans = ','.join(sorted(max(cliques, key=len)))
+print(ans)
+input_data.close()
+'''
+
+def backtrack(current_clique, candidates, processed_vertices, graph):
+	if not candidates and not processed_vertices:
+		if len(current_clique) > 2:
+			cliques.append(list(current_clique))
+		return
+	while candidates:
+		vertex = list(candidates)[0]
+		new_clique = current_clique.union({vertex})
+		new_candidates = candidates.intersection(graph[vertex])
+		new_processed_vertices = processed_vertices.intersection(graph[vertex])
+		backtrack(new_clique, new_candidates, new_processed_vertices, graph)
+		candidates.remove(vertex)
+		processed_vertices.add(vertex)
+	
+input_data = open('/home/advent-of-code-2024/Day23/advent_of_code_23.txt','r')
+input_data_lst = [x.replace('\n','').split('-') for x in input_data]
+graph = {}
+for pair in input_data_lst:
+	left, right = pair[0], pair[1]
+	if left not in graph:
+		graph[left] = []
+	graph[left].append(right)
+	if right not in graph:
+		graph[right] = []
+	graph[right].append(left)
+cliques = []
+current_clique = set()
+candidates = set(graph.keys())
+processed_vertices = set()
+backtrack(current_clique, candidates, processed_vertices, graph)
 ans = ','.join(sorted(max(cliques, key=len)))
 print(ans)
 input_data.close()
